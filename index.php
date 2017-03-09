081d483a9bd9c00be4f46ff9d4387042716f6e44
tommy koning <99039195@mydavinci.nl>
2017-03-09 12:00:51 +0100

    bijna klaar (hoopelijk)

--- index.php
+++ index.php
@@ -2,12 +2,34 @@
 <html>
 	<head>
 		<title>good morning</title>
+		<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script>
 	</head>
 	<body>
+		<script type="text/javascript"> 
+			var timeout = setTimeout(reload, 1000); 
+			function reload() {
+				$("#reload").load('index.php #reload', function(){
+					timeout = setTimeout(reload, 1000);
+				});
+			}
+		</script>
+		<div id="reload">
+				<?php
+					date_default_timezone_set('Europe/Amsterdam');
+					$time = date("h:i:s");
+					$pretext = "het is $time"; 
+					$timeday = "";
+					$text = $pretext + $timeday;
+					echo "hello world <br>";
 
-		<?php
-			echo "hello world";
-		?>
+					if ($time >="06:00" && $time < "12:00") {
+						$text = "goede morgen, ";
+						echo "$text";
+						echo ""
+					}
 
+
+				?>
+		</div>
 	</body>
 </html>
