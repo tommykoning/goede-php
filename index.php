960c567b6e361552d37455b60ccda029c94a42e1
tommy koning <99039195@mydavinci.nl>
2017-03-16 16:05:43 +0100

    done

--- index.php
+++ index.php
@@ -1,10 +1,35 @@
+<?php 
+	$timeZone = date_default_timezone_set('Europe/Amsterdam');
+	$time = date("H:i:s");
+	$pretext = "het is $time"; 
+	$world = "hello world <br>";
+
+	if ($time >="06:00" && $time < "12:00") {
+		$timeday = "goede morgen, $pretext";
+		$className = "morgen";
+	} elseif ($time >= "12:00" && $time < "18:00") {
+		$timeday = "goede middag, $pretext";
+		$className ="afternoon";	
+	} elseif ($time >= "18:00" && $time < "00:00") {
+		$timeday = "goede avond, $pretext";
+		$className = "evening";
+	} elseif ($time >= "00:00" && $time < "06:00") {
+		$timeday = "goede nacht, $pretext";
+		$className = "night";
+	} else {
+		$timeday = "error, $pretext"; 
+
+	};
+
+ ?>
 <!DOCTYPE html>
 <html>
 	<head>
 		<title>good morning</title>
 		<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script>
+		<link rel="stylesheet" type="text/css" href="index.css">
 	</head>
-	<body>
+	<body class = "<?php echo $className; ?>">
 		<script type="text/javascript"> 
 			var timeout = setTimeout(reload, 1000); 
 			function reload() {
@@ -15,20 +40,7 @@
 		</script>
 		<div id="reload">
 				<?php
-					date_default_timezone_set('Europe/Amsterdam');
-					$time = date("h:i:s");
-					$pretext = "het is $time"; 
-					$timeday = "";
-					$text = $pretext + $timeday;
-					echo "hello world <br>";
-
-					if ($time >="06:00" && $time < "12:00") {
-						$text = "goede morgen, ";
-						echo "$text";
-						echo ""
-					}
-
-
+					echo $timeday;	
 				?>
 		</div>
 	</body>
